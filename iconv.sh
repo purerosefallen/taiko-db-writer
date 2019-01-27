3cd4a5ea17f47f9096e9d8f1c91a45d7db3d4ab4
purerosefallen <78877@qq.com>
2019-01-27 12:19:20 +0800

    shs

---
+++ iconv.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+if [ "$#" != "2" ]; then
+echo "Usage: `basename $0` dir filter"
+exit
+fi
+dir=$1
+filter=$2
+echo $1
+for file in `find $dir -name "$2"`; do
+echo "$file"
+iconv -f SHIFT-JIS -t utf8 -o $file $file
+done
