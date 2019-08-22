6c4e581f9e67cc4045852986b393704f75f0e47a
purerosefallen <78877@qq.com>
2019-08-23 01:11:50 +0800

    fix

--- zip.sh
+++ zip.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-ls *.zip | xargs -I {} bash -c 'export PATH1=/tmp/$RANDOM ; export PATH2=$RANDOM ; 7z x -o$PATH1 {} ; mkdir $PATH2 ; mv $PATH1/*.ogg $PATH2/main.ogg ; mv $PATH1/*.tja $PATH2/main.tja'
+ls *.zip | xargs -I {} bash -c 'export PATH1=/tmp/$RANDOM ; export PATH2=$RANDOM ; 7z x -o$PATH1 "{}" ; mkdir $PATH2 ; mv $PATH1/*.ogg $PATH2/main.ogg ; mv $PATH1/*.tja $PATH2/main.tja'
