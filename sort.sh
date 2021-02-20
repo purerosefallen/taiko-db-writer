3300379258c74850a9ba7358ca3ebd4ad44fd963
purerosefallen <78877@qq.com>
2021-02-20 21:26:40 +0800

    update

--- sort.sh
+++ sort.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-find . -name '*.tja' | sed "s/.tja//g" | xargs -I {} bash -c 'export DIR_NAME=$RANDOM ; mkdir $DIR_NAME ; cp -rf "{}.tja" $DIR_NAME/main.tja ; cp -rf "{}.ogg" $DIR_NAME/main.ogg'
+find . -name '*.tja' | sed "s/.tja//g" | xargs -d "\n" -I {} bash -c 'export DIR_NAME=$RANDOM ; mkdir $DIR_NAME ; cp -rf "{}.tja" $DIR_NAME/main.tja ; cp -rf "{}.ogg" $DIR_NAME/main.ogg'
